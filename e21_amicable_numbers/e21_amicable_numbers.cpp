c2278da4884927523742f42930efe2df47d22001
Nikolay Nikolov <spoon_db@abv.bg>
2016-07-06 22:43:57 +0300

    e21 & e58

---
+++ e21_amicable_numbers/e21_amicable_numbers.cpp
@@ -0,0 +1,57 @@
+﻿//	Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+//	If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+//
+//	For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. 
+//	The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+//
+//	Evaluate the sum of all the amicable numbers under 10000.
+
+#include <iostream>
+#include <set>
+
+#define UPPER_LIMIT 10000
+
+int sum_of_divisors(int i);
+
+int main()
+{
+	std::set<int> amicable_nums;
+	std::set<int>::iterator it;
+
+	int n = UPPER_LIMIT;
+	for (int i = 2; i < n; i++)
+	{
+		int sum = sum_of_divisors(i);
+		int candidate = sum_of_divisors(sum);
+
+		if (i == candidate && candidate != sum)
+		{
+			amicable_nums.insert(i);
+			amicable_nums.insert(candidate);			
+		}
+	}
+
+	int k = 0;
+	for (it = amicable_nums.begin(); it != amicable_nums.end(); it++)
+	{
+		k += *it;
+	}
+
+	std::cout << k << std::endl;
+
+	return 0;
+}
+
+int sum_of_divisors(int i)
+{
+	int result = 0;
+	for (int j = 1; j < i; j++)
+	{
+		if (i % j == 0)
+		{
+			result += j;
+		}
+	}
+
+	return result;
+}
---
+++ e58_spiral_primes/e58_spiral_primes.cpp
@@ -0,0 +1,96 @@
+﻿//Starting with 1 and spiralling anticlockwise in the following way, a square spiral with side length 7 is formed.
+// 
+//		37 36 35 34 33 32 31
+//		38 17 16 15 14 13 30
+//		39 18  5  4  3 12 29
+//		40 19  6  1  2 11 28
+//		41 20  7  8  9 10 27
+//		42 21 22 23 24 25 26
+//		43 44 45 46 47 48 49
+//
+// It is interesting to note that the odd squares lie along the bottom right diagonal, 
+// but what is more interesting is that 8 out of the 13 numbers lying along both diagonals are prime;that is, a ratio of 8 / 13 ≈ 62 %.
+//
+// If one complete new layer is wrapped around the spiral above, a square spiral with side length 9 will be formed.
+// If this process is continued, what is the side length of the square spiral for which the ratio of primes along both diagonals 
+// first falls below 10 % ?
+
+#include <iostream>
+#include <math.h>
+
+#define TARGET_PERCENTAGE 10
+#define CUBE_SIZES 4 
+
+int is_prime(int);
+void print_statistics(int, double, double, double);
+
+int main()
+{
+	int cube_size = 3, num = 0;
+	double counter_diagonals = 1, counter_primes = 0, percentage = 0;
+
+	for (;;)
+	{
+		for (int i = CUBE_SIZES; i >= 1; i--)
+		{
+			num = (cube_size * cube_size) - ((cube_size - 1) * i);
+			counter_diagonals++;
+
+			if (is_prime(num)) counter_primes++;
+		}
+
+		percentage = (counter_primes / counter_diagonals) * 100;
+
+		if (percentage < TARGET_PERCENTAGE)
+		{
+			break;
+		}
+
+		cube_size += 2;
+	}
+
+	print_statistics(cube_size, counter_primes, counter_diagonals, percentage);
+
+	return 0;
+}
+
+int is_prime(int num)
+{
+	if (num < 2)
+	{
+		return 0;
+	}
+
+	if (num == 3 || num == 2)
+	{
+		return 1;
+	}
+
+	if (num % 2 == 0 || num % 3 == 0)
+	{
+		return 0;
+	}
+
+	int i;
+	for (i = 5; i * i <= num; i += 6)
+	{
+		if (num % i == 0 || num % (i + 2) == 0)
+		{
+			return 0;
+		}
+	}
+
+	return 1;
+}
+
+void print_statistics(int cube_size, double counter_primes, double counter_diagonals, double percentage)
+{
+	std::cout << " --- Cube size: ";
+	std::cout << cube_size << std::endl;
+	std::cout << " --- Primes: ";
+	std::cout << counter_primes << std::endl;
+	std::cout << " --- Counter diagonals: ";
+	std::cout << counter_diagonals << std::endl;
+	std::cout << " --- Percentage: ";
+	std::cout << percentage << std::endl;
+}
