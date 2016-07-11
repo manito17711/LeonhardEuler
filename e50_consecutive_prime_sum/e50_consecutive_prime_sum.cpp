c0702b30439440777c082679c5c111389da62350
Nikolay Nikolov <spoon_db@abv.bg>
2016-07-11 20:16:11 +0300

    update data type

--- e50_consecutive_prime_sum/e50_consecutive_prime_sum.cpp
+++ e50_consecutive_prime_sum/e50_consecutive_prime_sum.cpp
@@ -1,49 +1,45 @@
 // The prime 41, can be written as the sum of six consecutive primes:
-//
+// 
 // 41 = 2 + 3 + 5 + 7 + 11 + 13
 // This is the longest sum of consecutive primes that adds to a prime below one - hundred.
-//
+// 
 // The longest sum of consecutive primes below one - thousand that adds to a prime, contains 21 terms, and is equal to 953.
-//
+// 
 // Which prime, below one - million, can be written as the sum of the most consecutive primes ?
 
 #include <iostream>
+#include <vector>
 
-using namespace std;
+#define ONE_MILLION 1000000
 
-int is_prime(const int);
+int is_prime(int);
 
 int main()
 {
-	int n = 3, top_n = 1000000, arr_length = 78500, arr_counter = 0;
-	int arr[78500] = { 0 };
-	arr[arr_counter++] = 2;
+	std::vector<int> primes;
+	primes.push_back(2); // firts prime
+	int n = 3; // second prime
 
-	while (n < top_n)
+	while (n < ONE_MILLION)
 	{
 		if (is_prime(n))
 		{
-			arr[arr_counter++] = n;
+			primes.push_back(n);
 		}
 
 		n += 2;
 	}
 
 	int sum, t_count, r_num, r_count = 0;
-	for (int i = 0; i < arr_length; i++)
+	for (size_t i = 0; i < primes.size(); i++)
 	{
-		sum = arr[i];
+		sum = primes[i];
 		t_count = 1;
 
-		for (int j = i + 1; j < arr_length; j++)
+		for (size_t j = i + 1; j < primes.size(); j++)
 		{
-			if (arr[j] == 0)
-			{
-				break;
-			}
-
-			sum += arr[j];
-			if (sum > top_n)
+			sum += primes[j];
+			if (sum > ONE_MILLION)
 			{
 				break;
 			}
@@ -61,21 +57,21 @@ int main()
 		}
 	}
 
-	cout << "r_num: " << r_num << ", r_count: " << r_count << endl;
+	std::cout << r_num << std::endl;
 
 	return 0;
 }
 
-int is_prime(const int num) 
+int is_prime(int num)
 {
-	if (num == 2 || num == 3)
-	{
-		return 1;
-	}
-
 	if (num < 2)
 	{
 		return 0;
+	}
+
+	if (num == 3 || num == 2)
+	{
+		return 1;
 	}
 
 	if (num % 2 == 0 || num % 3 == 0)
@@ -94,4 +90,3 @@ int is_prime(const int num)
 
 	return 1;
 }
-
