18063980d638dbdbc0ee46c6afa0a9f2e5c2e63a
Nikolay Nikolov <spoon_db@abv.bg>
2016-07-16 14:02:43 +0300

    e48 self powers

--- e48_self_powers/e48_self_powers.cpp
+++ e48_self_powers/e48_self_powers.cpp
@@ -1,3 +1,7 @@
+// The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+// 
+// Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+
 #include <iostream>
 
 #define LAST_N_DIGITS 10000000000
