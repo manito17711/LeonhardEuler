a4e388389e14d24195a0efbe8a0745e1da168efc
Nikolay Nikolov <spoon_db@abv.bg>
2016-07-16 14:01:02 +0300

    e48 self powers

---
+++ e48_self_powers/e48_self_powers.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+#define LAST_N_DIGITS 10000000000
+
+typedef unsigned long long uul;
+
+uul fact(int);
+
+int main()
+{
+	uul num = 0;
+	int max_pwr = 1000;
+	for (int i = 1; i <= max_pwr; i++)
+	{
+		num += fact(i);
+		num %= LAST_N_DIGITS; // we are getting the last N digits from the number and cutting the rest..
+	}
+
+	std::cout << num << std::endl;
+
+	return 0;
+}
+
+uul fact(int i)
+{
+	uul f = i;
+	for (int j = 1; j < i; j++)
+	{
+		f *= i;
+		f %= LAST_N_DIGITS;
+	}
+
+	return f;
+}
+
